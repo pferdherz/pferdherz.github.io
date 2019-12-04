2b573df439ac245dfaa34fd7eb77a2d7ee0312a1
Mirco Kocher <mirco.kocher@gmx.ch>
2019-12-04 21:06:35 +0100

    news

--- txt/news.js
+++ txt/news.js
@@ -2,3 +2,43 @@
 var news_titel = "Aktuelles";
 
 var news_text1 = "blabla";
+
+var news_column1_titel = "J Kurse";
+
+var news_column1_text1 = "blabla";
+
+var news_column1_text2 = "blabla";
+
+var news_column1_text3 = "blabla";
+
+var news_column1_text4 = "blabla";
+
+var news_column2_titel = "Erle";
+
+var news_column2_text1 = "blabla";
+
+var news_column2_text2 = "blabla";
+
+var news_column2_text3 = "blabla";
+
+var news_column2_text4 = "blabla";
+
+var news_column3_titel = "Preisliste";
+
+var news_column3_text1 = "blabla";
+
+var news_column3_text2 = "blabla";
+
+var news_column3_text3 = "blabla";
+
+var news_column3_text4 = "blabla";
+
+var news_column4_titel = "Sonstiges";
+
+var news_column4_text1 = "blabla";
+
+var news_column4_text2 = "blabla";
+
+var news_column4_text3 = "blabla";
+
+var news_column4_text4 = "blabla";

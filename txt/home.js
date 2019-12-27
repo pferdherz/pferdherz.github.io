53e6fbdfb143344f0acaa7a90f071bda3e1b14ea
pferdherz <58362627+pferdherz@users.noreply.github.com>
2019-12-27 14:45:33 +0100

    Update home.js

--- txt/home.js
+++ txt/home.js
@@ -2,5 +2,5 @@
 var home_titel = "Herzlich Willkommen auf der Homepage von rlb-reiten";
 
 var home_text1 = "Wir bieten Kinderreitstunden, mobiler Reitunterricht, Bodenarbeit, Heilpädagogisches Reiten und SURE FOOT <sup>&reg;</sup> Einheiten an."; 
-var home_rahel_text1 = "Hallo, ich bin Rahel. Mein Angebot ist: Kinderreitstunden, Heilpädagogosches Reiten";
-var home_laura_text1 = "Hallo, ich bin Laura: Mein Angebot ist: Kinderreitstunden, mobiler Reitunterricht, Bodenarbeit und SURE FOOT <sup>&reg;</sup> Einheiten";
+var home_rahel_text1 = "Hallo, ich bin Rahel. Mein Angebot: <b> Kinderreitstunden <b> Heilpädagogosches Reiten";
+var home_laura_text1 = "Hallo, ich bin Laura: Mein Angebot: <b> Kinderreitstunden <b> mobiler Reitunterricht <b> Bodenarbeit <b> SURE FOOT <sup>&reg;</sup> Einheiten";

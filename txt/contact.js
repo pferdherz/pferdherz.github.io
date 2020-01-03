92f1831d57b91c43b32d444d4130cc48dd0c9d42
pferdherz <58362627+pferdherz@users.noreply.github.com>
2020-01-03 15:37:59 +0100

    Update contact.js

--- txt/contact.js
+++ txt/contact.js
@@ -1,28 +1,26 @@
 
 var contact_titel = "Kontakt";
 
-var contact_text1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+var contact_text1 = " ";
+var contact_rahel_title = "Rahel Bürgy";
 
-var contact_rahel_title = "Rahel B";
+var contact_rahel_text1 = " ";
 
-var contact_rahel_text1 = "Musterweg 7";
+var contact_rahel_text2 = "3267 Seedorf";
 
-var contact_rahel_text2 = "1234 Musterort";
+var contact_rahel_text3 = " ";
+var contact_rahel_phone = "+41 79 321 90 71";
 
-var contact_rahel_text3 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+var contact_rahel_mail = "rahel.buergy@outlook.com";
 
-var contact_rahel_phone = "+41 98 765 43 21";
+var contact_laura_title = "Laura Bigler";
 
-var contact_rahel_mail = "rahel.b@pferdli.ch";
+var contact_laura_text1 = " ";
 
-var contact_laura_title = "Laura B";
+var contact_laura_text2 = "3254 Schüpfen";
 
-var contact_laura_text1 = "Musterstrasse 4";
+var contact_laura_text3 = " ";
 
-var contact_laura_text2 = "9876 Musterstadt";
+var contact_laura_phone = "+41 79 482 42 59";
 
-var contact_laura_text3 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
-
-var contact_laura_phone = "+41 12 345 67 89";
-
-var contact_laura_mail = "laura.b@pferdli.ch";
+var contact_laura_mail = "l.bigler@hotmail.com";

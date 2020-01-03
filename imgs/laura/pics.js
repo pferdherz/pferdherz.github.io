ceddde0bacfd9231db83bb9a60a402a9b7a037cc
pferdherz <58362627+pferdherz@users.noreply.github.com>
2020-01-03 16:44:33 +0100

    Update pics.js

--- imgs/laura/pics.js
+++ imgs/laura/pics.js
@@ -3,6 +3,6 @@ var laura_column1_pic = "titelbild";
 
 var laura_column2_pic = "unterricht2";
 
-var laura_column3_pic = "bodenarbeit";
+var laura_column3_pic = "boden2";
 
 var laura_column4_pic = "vally pads";

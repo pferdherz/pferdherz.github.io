6dadde92a79c988c4e3b07c0cf8cfaf11c61a73c
pferdherz <58362627+pferdherz@users.noreply.github.com>
2020-01-03 13:57:44 +0100

    Update rahel.js

--- txt/rahel.js
+++ txt/rahel.js
@@ -1,52 +1,52 @@
 
 var rahel_titel = "Rahel";
 
-var rahel_text1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+var rahel_text1 = "Diese Seite befindet sich im Aufbau.";
 
 var rahel_column1_titel = "Über mich";
 
-var rahel_column1_text1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+var rahel_column1_text1 = " ";
 
-var rahel_column1_text2 = "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+var rahel_column1_text2 = " ";
 
-var rahel_column1_text3 = "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
+var rahel_column1_text3 = " ";
 
-var rahel_column1_text4 = "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+var rahel_column1_text4 = " ";
 
 var rahel_column1_text5 = "";
 
-var rahel_column2_titel = "Heil Reiten";
+var rahel_column2_titel = "Kinderreitstunden";
 
-var rahel_column2_text1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+var rahel_column2_text1 = " ";
 
-var rahel_column2_text2 = "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+var rahel_column2_text2 = " ";
 
-var rahel_column2_text3 = "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
+var rahel_column2_text3 = " ";
 
-var rahel_column2_text4 = "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+var rahel_column2_text4 = " ";
 
 var rahel_column2_text5 = "";
 
-var rahel_column3_titel = "Kinder";
+var rahel_column3_titel = "Heilpädagogisches Reiten";
 
-var rahel_column3_text1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+var rahel_column3_text1 = " ";
 
-var rahel_column3_text2 = "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+var rahel_column3_text2 = " ";
 
-var rahel_column3_text3 = "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
+var rahel_column3_text3 = " ";
 
-var rahel_column3_text4 = "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+var rahel_column3_text4 = " ";
 
 var rahel_column3_text5 = "";
 
-var rahel_column4_titel = "Erlebnis";
+var rahel_column4_titel = "";
 
-var rahel_column4_text1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+var rahel_column4_text1 = " ";
 
-var rahel_column4_text2 = "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+var rahel_column4_text2 = " ";
 
-var rahel_column4_text3 = "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur";
+var rahel_column4_text3 = " ";
 
-var rahel_column4_text4 = "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+var rahel_column4_text4 = " ";
 
 var rahel_column4_text5 = "";

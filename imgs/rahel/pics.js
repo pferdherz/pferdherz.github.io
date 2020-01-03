0722cf44459e5c33dc810c508a04608ffbbfc512
pferdherz <58362627+pferdherz@users.noreply.github.com>
2020-01-03 15:33:15 +0100

    Update pics.js

--- imgs/rahel/pics.js
+++ imgs/rahel/pics.js
@@ -1,8 +1,8 @@
 
 var rahel_column1_pic = "rahel titel";
 
-var rahel_column2_pic = "Heil";
+var rahel_column2_pic = 
 
-var rahel_column3_pic = "Kinder";
+var rahel_column3_pic = 
 
-var rahel_column4_pic = "Erlebnis";
+var rahel_column4_pic = 

4126063927a0551055ea8b2703b58020623a288c
Mirco Kocher <mirco.kocher@gmx.ch>
2020-01-04 13:35:16 +0100

    responsive image grid in gallery

--- imgs/gallery/pics.js
+++ imgs/gallery/pics.js
@@ -1,2 +1,8 @@
 
-var pics = ["kurs", "agility", "springen", "avenches", "abend2", "tofflan"];
+var pics1 = ["tofflan", "kurs", "abend2", "jura"];
+
+var pics2 = ["springen", "agility", "hut", "tofflan"];
+
+var pics3 = ["jura", "abend2", "springen", "avenches"];
+
+var pics4 = ["agility", "hut", "avenches", "kurs"];

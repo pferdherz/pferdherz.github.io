8e8b235b3a981cfea38295aa5ff966bc65f7461d
pferdherz <58362627+pferdherz@users.noreply.github.com>
2020-01-12 19:18:31 +0100

    Update news.js

--- txt/news.js
+++ txt/news.js
@@ -5,7 +5,7 @@ var news_text1 = "Hier finden Sie akutelle Informationen zu J&S Kursen, Reittage
 var news_column1_titel = "J&S Kurse";
 
 var news_column1_text1 = "Ihr Kind lernt spielerisch das Pferd kennen. Dabei lernen wir, wie das Pferd richtig gepflegt wird, wie man ein Pferd führt und reitet. Dabei lernen wir das Reiten auf dem Platz, das Voltigieren aber auch das Reiten im Gelände kennen.";
-var news_column1_text2 = "Der nächste J&S Gruppenkurs startet am 12. Februar 2020 und findet jeweils an 2 - 3  Mittwochnachmittagen pro Monat von 14.15 - 15.45 Uhr statt. Es hat noch freie Plätze und Anmeldungen werden noch entgegen genommen. Für eine komplette Ausschreibung oder bei Fragen dürfen Sie sich gerne unverbindlich melden. <br> Daten Quartal 1: 12.1, 19.2, 26.2, 11.3, 18.3, 25.3 <br> Daten Quartal 2: 22.4, 6.5, 13.5, 20.5, 17.6, 1.7";
+var news_column1_text2 = "Der nächste J&S Gruppenkurs startet am 12. Februar 2020 und findet jeweils an 2 - 3  Mittwochnachmittagen pro Monat von 14.15 - 15.45 Uhr statt. Es hat noch freie Plätze und Anmeldungen werden noch entgegen genommen. Für eine komplette Ausschreibung oder bei Fragen dürfen Sie sich gerne unverbindlich melden. <br><br> Daten Quartal 1: 12.1, 19.2, 26.2, 11.3, 18.3, 25.3 <br><br> Daten Quartal 2: 22.4, 6.5, 13.5, 20.5, 17.6, 1.7";
 
 var news_column1_text3 = " ";
 
@@ -22,7 +22,7 @@ var news_column2_text4 = " ";
 
 var news_column3_titel = "Preisliste";
 
-var news_column3_text1 = "Kinderreitstunde Privat <br>45.- <br><br> Erwachsene Privta<br> 50 Min. 50.- <br><br> SURE FOOT <br><br>50-60 Min. 60.- <br> 3er Abo 160.- <br><br>Bei mobilem Unterricht kommen Fahrkosten dazu. <br><br>AGB<br>Im Verhinderungsfall bitte 24 Stunden im Voraus abmelden, sonst muss die Stunde verechnet werden.<br><br>Das Tragen eines Reithelms für Kinder ist Pflicht.<br><br>Es wird jede Haftung abgelehnt.";
+var news_column3_text1 = "Kinderreitstunde Privat <br>45.- <br><br> Erwachsene Privat<br> 50.- (50 Min.) <br><br> SURE FOOT<br>60.- (50-60 Min) <br> 3er Abo 160.- <br><br>Bei mobilem Unterricht kommen Fahrkosten dazu. <br><br>AGB<br>Im Verhinderungsfall bitte 24 Stunden im Voraus abmelden, sonst muss die Stunde verechnet werden.<br><br>Das Tragen eines Reithelms für Kinder ist Pflicht.<br><br>Das Aufbauen und Aufräumen von Hindernissen wird grundsätzlich in die Lektionenzeit eingerechnet. Geimeinsam geht es schneller.<br><br> Es wird jegliche Haftung abgelehnt.";
 
 var news_column3_text2 = " ";
 

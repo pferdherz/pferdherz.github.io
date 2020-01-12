d954560d2781edc6b6cef6ebb4d0a2ad6dc9c38d
pferdherz <58362627+pferdherz@users.noreply.github.com>
2020-01-12 13:32:12 +0100

    Update news.js

--- txt/news.js
+++ txt/news.js
@@ -22,7 +22,7 @@ var news_column2_text4 = " ";
 
 var news_column3_titel = "Preisliste";
 
-var news_column3_text1 = "Kinderreitstunde Privat 45.- <br> Privat Erwachsene 50 Min. 50.- <br> SURE FOOT Einheit 50-60 Min. 60.- <br> Bei mobilem Unterricht kommen noch Fahrkosten dazu. ";
+var news_column3_text1 = "Kinderreitstunde Privat 45.- <br> Privat Erwachsene 50 Min. 50.- <br> SURE FOOT 50-60 Min. 60.- <br> Bei mobilem Unterricht kommen noch Fahrkosten dazu. ";
 
 var news_column3_text2 = " ";
 

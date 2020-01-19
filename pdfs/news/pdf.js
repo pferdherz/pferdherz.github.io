4e64b07a8ceba07d0723e59a2a3041dc4723e05b
pferdherz <58362627+pferdherz@users.noreply.github.com>
2020-01-19 17:26:43 +0100

    Update pdf.js

--- pdfs/news/pdf.js
+++ pdfs/news/pdf.js
@@ -1,23 +1,23 @@
 
 var news_column1_pdf1 = "Anmeldung J&S 2020";
 
-var news_column1_pdf2 = "Beispiel-1-2";
+var news_column1_pdf2 = "";
 
 var news_column1_pdf3 = "";
 
-var news_column2_pdf1 = "Beispiel-2-1";
+var news_column2_pdf1 = " ";
 
-var news_column2_pdf2 = "Beispiel-2-2";
+var news_column2_pdf2 = " ";
 
-var news_column2_pdf3 = "Beispiel-2-3";
+var news_column2_pdf3 = " ";
 
-var news_column3_pdf1 = "Beispiel-3-1";
+var news_column3_pdf1 = " ";
 
 var news_column3_pdf2 = "";
 
 var news_column3_pdf3 = "";
 
-var news_column4_pdf1 = "Beispiel-4-1";
+var news_column4_pdf1 = " ";
 
 var news_column4_pdf2 = "";
 

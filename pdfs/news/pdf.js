852a98f848e6af371738f3c0ebeb214df5e66c72
Mirco Kocher <mirco.kocher@gmx.ch>
2020-01-19 17:06:44 +0100

    pdf in news

---
+++ pdfs/news/pdf.js
@@ -0,0 +1,24 @@
+
+var news_column1_pdf1 = "Beispiel-1-1";
+
+var news_column1_pdf2 = "Beispiel-1-2";
+
+var news_column1_pdf3 = "";
+
+var news_column2_pdf1 = "Beispiel-2-1";
+
+var news_column2_pdf2 = "Beispiel-2-2";
+
+var news_column2_pdf3 = "Beispiel-2-3";
+
+var news_column3_pdf1 = "Beispiel-3-1";
+
+var news_column3_pdf2 = "";
+
+var news_column3_pdf3 = "";
+
+var news_column4_pdf1 = "Beispiel-4-1";
+
+var news_column4_pdf2 = "";
+
+var news_column4_pdf3 = "";

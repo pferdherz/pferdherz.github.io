bfdb45b5777bdb99a8ce7f0fadca639075d8b016
pferdherz <58362627+pferdherz@users.noreply.github.com>
2020-01-27 22:03:15 +0100

    Update rahel.js

--- txt/rahel.js
+++ txt/rahel.js
@@ -15,11 +15,11 @@ var rahel_column1_text4 = " ";
 
 var rahel_column1_text5 = "";
 
-var rahel_column2_titel = "Reitstunden";
+var rahel_column2_titel = "Kinderreiten";
 
-var rahel_column2_text1 = "Diese Rubrik befindet sich im Aufbau";
+var rahel_column2_text1 = "Spaziergänge und Ausritte<br> Reiten auf dem Platz und an der Longe";
 
-var rahel_column2_text2 = " ";
+var rahel_column2_text2 = "In Baggwil (3267 Seedorf) biete ich erlebnisreiche und freudige Stunden bei den Pferden an. Im Vordergrund steht der Umgang mit dem Pferd. Wir bauen eine Beziehung und Freundschaft zum Pferd auf. Wir führen, putzen und pflegen das Pferd. Erledigen kleinere Stallarbeiten sowie füttern und belohnen das Pferd.<br>Kinder ab 3 Jahren lernen spielerisch den Umgang mit dem Pferd. Dabei unternehmen wir Ausritte/Spaziergänge übers Feld oder in den Wald. Auch das Reiten auf dem Reitplatz kommt nicht zu kurz. Das Reiten findet alleine oder zu zweit mit einem Pferd statt.<br>Auch Bodenarbeit und Reiten an der Longe gehören dazu.<br>Wer sattelfest ist, darf auch auf dem Handpferd oder frei mit ins Gelände.";
 
 var rahel_column2_text3 = " ";
 
@@ -27,11 +27,11 @@ var rahel_column2_text4 = " ";
 
 var rahel_column2_text5 = "";
 
-var rahel_column3_titel = "Heilp. Reiten";
+var rahel_column3_titel = "Heilpädagogisches Reiten";
 
-var rahel_column3_text1 = "Die Rubrik befindet sich im Aufbau";
+var rahel_column3_text1 = "Spaziergänge und Ausritte<br> Reiten auf dem Platz";
 
-var rahel_column3_text2 = " ";
+var rahel_column3_text2 = "Heilpädagogisches Reiten (im Folgenden kurz „HPR“ genannt) ist eine ganzheitliche Fördermethode. Das heisst der Mensch wird auf körperlicher, emotionaler und geistiger Ebene angesprochen. Wer mit einem Pferd in Kontakt tritt, macht eine Ganzheitserfahrung. Durch äussere Einwirkungen kann das Innere berührt oder gar verändert werden. Und eines ist klar, Pferde sprechen das Innere an, sie berühren unseren Gefühlsbereich und rufen viele verschiedene Gefühle hervor. Das Selbstbewusstsein wird gestärkt, das Gleichgewicht, die Motorik und die Beweglichkeit verbessert.<br>Wobei kann HPR helfen:<br><li>Gefühlslage</li><br><li>Schulung der Wahrnehmung</li><br><li>Motorik</li><br><li>Sozial-inegrativer Bereich</li><br><li>Kommunikationsbereich</li><br><li>Bewegungskoordination und Gleichgewicht</li><br>";
 
 var rahel_column3_text3 = " ";
 

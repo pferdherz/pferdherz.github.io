e1fef4a7aa3aebaea86a471065a20db8e7930a69
pferdherz <58362627+pferdherz@users.noreply.github.com>
2020-03-17 07:42:01 +0100

    Update news.js

--- txt/news.js
+++ txt/news.js
@@ -4,7 +4,7 @@ var news_titel = "Aktuelles";
 var news_text1 = "Hier finden Sie akutelle Informationen zu J&S Kursen, Reittagen, Preislisten und weitere News.";
 var news_column1_titel = "J&S Kurse";
 
-var news_column1_text1 = "Ihr Kind lernt spielerisch das Pferd kennen. Dabei lernen wir, wie das Pferd richtig gepflegt wird, wie man ein Pferd führt und reitet. Dabei lernen wir das Reiten auf dem Platz, das Voltigieren aber auch das Reiten im Gelände kennen.";
+var news_column1_text1 = "Der Kurs findet zur Zeit nicht statt. Wir hoffen ab dem 20.04.2020 wieder startet zu können. Wir halten Sie auf dem Laufenden.<br><br>Ihr Kind lernt spielerisch das Pferd kennen. Dabei lernen wir, wie das Pferd richtig gepflegt wird, wie man ein Pferd führt und reitet. Dabei lernen wir das Reiten auf dem Platz, das Voltigieren aber auch das Reiten im Gelände kennen.";
 var news_column1_text2 = "Der nächste J&S Gruppenkurs startet am 12. Februar 2020 und findet jeweils an 2 - 3  Mittwochnachmittagen pro Monat von 14.15 - 15.45 Uhr statt. <br><br>Der Kurs hat gestartet. Laufender Einstieg ist möglich. <br><br>Für eine komplette Ausschreibung oder bei Fragen dürfen Sie sich gerne unverbindlich melden. <br><br> Daten Quartal 1: 12.1, 19.2, 26.2, 11.3, 18.3, 25.3 <br><br> Daten Quartal 2: 22.4, 6.5, 13.5, 20.5, 17.6, 1.7";
 
 var news_column1_text3 = " ";
@@ -32,7 +32,7 @@ var news_column3_text4 = " ";
 
 var news_column4_titel = "Sonstiges";
 
-var news_column4_text1 = "NEUES DATUM! <br> Haben Sie noch keine Pläne für die Frühlingsferien? <br>Wie wäre es mit dem MS Sport Reitcamp im Reitstall Bernerschachen in Attiswil <br>Mo 06.04.20 - Do 09.04.20. <br>Weitere Infos finden Sie auf www.bernerschachen.ch. <br>Das Team Bernerschachen und ich freuen uns auf euch. <br><br>11. April 2020<br>Gymkhana-Trail-Kurs in Hessigkofen.<br> - Mind. 12 Hindernissen<br> - SURE FOOT vor Ort testen";
+var news_column4_text1 = "MS Sport Camp in Attiswil <br> und <br>Gymkhana-Trail-Kurs in Hessigkofen findet nicht statt. <br>Es wird für ein neues Datum geschaut.";
 
 var news_column4_text2 = " ";
 

e1265333480ec7dfaea50305ce647892c3169f45
pferdherz <58362627+pferdherz@users.noreply.github.com>
2020-05-11 11:28:24 +0200

    Update news.js

--- txt/news.js
+++ txt/news.js
@@ -4,8 +4,8 @@ var news_titel = "Aktuelles";
 var news_text1 = "Hier finden Sie akutelle Informationen zu J&S Kursen, Reittagen, Preislisten und weitere News.";
 var news_column1_titel = "J&S Kurse";
 
-var news_column1_text1 = "Der Kurs findet zur Zeit nicht statt. Wir hoffen ab dem 20.04.2020 wieder startet zu können. Wir halten Sie auf dem Laufenden.<br><br>Ihr Kind lernt spielerisch das Pferd kennen. Dabei lernen wir, wie das Pferd richtig gepflegt wird, wie man ein Pferd führt und reitet. Dabei lernen wir das Reiten auf dem Platz, das Voltigieren aber auch das Reiten im Gelände kennen.";
-var news_column1_text2 = "Der nächste J&S Gruppenkurs startet am 12. Februar 2020 und findet jeweils an 2 - 3  Mittwochnachmittagen pro Monat von 14.15 - 15.45 Uhr statt. <br><br>Der Kurs hat gestartet. Laufender Einstieg ist möglich. <br><br>Für eine komplette Ausschreibung oder bei Fragen dürfen Sie sich gerne unverbindlich melden. <br><br> Daten Quartal 1: 12.1, 19.2, 26.2, 11.3, 18.3, 25.3 <br><br> Daten Quartal 2: 22.4, 6.5, 13.5, 20.5, 17.6, 1.7";
+var news_column1_text1 = "Der Kurs findet zur Zeit nicht statt. Wir hoffen ab Ende Mai / Anfangs Juni unter den Auflagen des BAGs wieder startet zu können. Wir halten Sie auf dem Laufenden.<br><br>Ihr Kind lernt spielerisch das Pferd kennen. Dabei lernen wir, wie das Pferd richtig gepflegt wird, wie man ein Pferd führt und reitet. Dabei lernen wir das Reiten auf dem Platz, das Voltigieren aber auch das Reiten im Gelände kennen.";
+var news_column1_text2 = "Der nächste J&S Gruppenkurs startet am 12. Februar 2020 und findet jeweils an 2 - 3  Mittwochnachmittagen pro Monat von 14.15 - 15.45 Uhr statt. <br><br>Der Kurs hat gestartet. Laufender Einstieg ist möglich. <br><br>Für eine komplette Ausschreibung oder bei Fragen dürfen Sie sich gerne unverbindlich melden.";
 
 var news_column1_text3 = " ";
 
@@ -13,7 +13,7 @@ var news_column1_text4 = " ";
 
 var news_column2_titel = "Kinderreittage";
 
-var news_column2_text1 = "Kinderreittage Baggwil <br><br> 04.04.20 Osterspass <br> 15.04.20 Frühlingsspass <br> <br> Eine schöne, sinnvolle und lustige Zeit mit dem Pferden verbringen. Wir freuen uns auf eine gute Zeit mit euch. <br><br>Die Kurse finden unter den Auflagen des BAG statt.";
+var news_column2_text1 = "Reittage im Frühling mussten abgesagt werden. Für den Sommer sind Kurse in Planung. Die Kurse finden unter den Auflagen des BAG statt.";
 var news_column2_text2 = " ";
 
 var news_column2_text3 = " ";
@@ -32,7 +32,7 @@ var news_column3_text4 = " ";
 
 var news_column4_titel = "Sonstiges";
 
-var news_column4_text1 = "MS Sport Camp in Attiswil <br> und <br>Gymkhana-Trail-Kurs in Hessigkofen finden nicht statt. <br><br>Der Gymkhana-Trail-Kurs findet an einem anderen Datum statt.";
+var news_column4_text1 = "Mobiler Privat- und Halbprivatunterricht mit eigenem Pferd wieder möglich.<br><br>Auflagen BAG<br>-Abstand halten<br>-wenn möglich via TWINT oder ebanking zahlen<br><br>Ich freue mich auf euch!";
 
 var news_column4_text2 = " ";
 

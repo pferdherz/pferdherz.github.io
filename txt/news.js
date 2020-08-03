2f946fbbe7c2f7ed81b5c487b395f200f1aadddd
pferdherz <58362627+pferdherz@users.noreply.github.com>
2020-08-03 17:12:56 +0200

    Update news.js

--- txt/news.js
+++ txt/news.js
@@ -4,7 +4,7 @@ var news_titel = "Aktuelles";
 var news_text1 = "Hier finden Sie akutelle Informationen zu J&S Kursen, Reittagen, Preislisten und weitere News.";
 var news_column1_titel = "J&S Kurse";
 
-var news_column1_text1 = "Zur Zeit findet kein Kurs statt. <br>Einzellektionen oder Lektionen in Kleingruppe am Montagnachmittag möglich. ";
+var news_column1_text1 = "Zur Zeit findet kein Kurs statt. <br>Einzellektionen oder Lektionen in Kleingruppe am Montagnachmittag möglich.<br><br>Bei Interesse stelle ich gerne ein passendes Programm zusammen! ";
 var news_column1_text2 = " ";
 var news_column1_text3 = " ";
 
@@ -21,7 +21,7 @@ var news_column2_text4 = " ";
 
 var news_column3_titel = "Preise & AGBs";
 
-var news_column3_text1 = "Kinderreitstunde Privat <br>45.- <br><br> Erwachsene Privat<br> 50.- (50 Min.) <br><br> SURE FOOT<sup>&reg;</sup><br>60.- (50-60 Min) <br> 3er Abo 160.- <br><br>Bei mobilem Unterricht kommen Fahrkosten dazu. <br><br>Im Verhinderungsfall bitte 24 Stunden im Voraus abmelden, sonst muss die Stunde verechnet werden.<br><br>Das Tragen eines Reithelms für Kinder ist Pflicht.<br><br>Das Aufbauen und Aufräumen von Hindernissen wird grundsätzlich in die Lektionenzeit eingerechnet. Geimeinsam geht es schneller.<br><br> Es wird jegliche Haftung abgelehnt.";
+var news_column3_text1 = "Kinderreitstunde Privat <br>45.- (50 Min) <br><br> Erwachsene Privat<br> 50.- (50 Min.) <br><br> SURE FOOT<sup>&reg;</sup><br>60.- (50-60 Min) <br> 3er Abo 160.- <br><br>Bei mobilem Unterricht kommen Fahrkosten dazu. <br><br>Im Verhinderungsfall bitte 24 Stunden im Voraus abmelden, sonst muss die Stunde verechnet werden.<br><br>Das Tragen eines Reithelms für Kinder ist Pflicht.<br><br>Das Aufbauen und Aufräumen von Hindernissen wird grundsätzlich in die Lektionenzeit eingerechnet. Geimeinsam geht es schneller.<br><br> Es wird jegliche Haftung abgelehnt.";
 
 var news_column3_text2 = " ";
 

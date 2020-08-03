47eeff3dbabd54702eae32ed4d532547e1424601
pferdherz <58362627+pferdherz@users.noreply.github.com>
2020-08-03 17:07:02 +0200

    Update news.js

--- txt/news.js
+++ txt/news.js
@@ -4,15 +4,15 @@ var news_titel = "Aktuelles";
 var news_text1 = "Hier finden Sie akutelle Informationen zu J&S Kursen, Reittagen, Preislisten und weitere News.";
 var news_column1_titel = "J&S Kurse";
 
-var news_column1_text1 = "Der Kurs ist wieder gestartet.<br><br>Ihr Kind lernt spielerisch das Pferd kennen. Dabei lernen wir, wie das Pferd richtig gepflegt wird, wie man ein Pferd führt und reitet. Dabei lernen wir das Reiten auf dem Platz, das Voltigieren aber auch das Reiten im Gelände kennen.";
-var news_column1_text2 = "Der Kurs endet im Juli 2020. Einzellektionen in und nach den Sommerferien sind möglich. Es wird geprüft, ob nach den Sommerferien ein weiterer Kurs statt findet.";
+var news_column1_text1 = "Zur Zeit findet kein Kurs statt. br>Einzellektionen oder Lektionen in Kleingruppe am Montagnachmittag möglich. ";
+var news_column1_text2 = " ";
 var news_column1_text3 = " ";
 
 var news_column1_text4 = " ";
 
 var news_column2_titel = "Kinderreittage";
 
-var news_column2_text1 = "Reittage im Frühling mussten abgesagt werden. Für den Sommer sind Kurse in Planung. Die Kurse finden unter den Auflagen des BAG statt.";
+var news_column2_text1 = "Reittage im Frühling mussten abgesagt werden. Für den Herbst sind Kurse in Planung. Die Kurse finden unter den Auflagen des BAG statt.";
 var news_column2_text2 = " ";
 
 var news_column2_text3 = " ";
@@ -31,7 +31,7 @@ var news_column3_text4 = " ";
 
 var news_column4_titel = "Sonstiges";
 
-var news_column4_text1 = "Sommer Reitlager <br> in Attiswil <br> www.bernerschachen.ch <br><br>Gymkhana-Stangen-Kurs <br> 26.08. Hessigkofen <br> 2 freie Plätze in <br> der 2. Gruppe!";
+var news_column4_text1 = "Stangen-Trail-Training in Hessigkofen.<br><br>12.09.20<br>03.10.20";
 var news_column4_text2 = " ";
 
 var news_column4_text3 = " ";

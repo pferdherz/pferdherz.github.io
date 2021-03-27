94cced75aabbd64b61e979a29131b08e99f5b84f
pferdherz <58362627+pferdherz@users.noreply.github.com>
2021-03-27 20:56:59 +0100

    Update pdf.js

--- pdfs/news/pdf.js
+++ pdfs/news/pdf.js
@@ -1,5 +1,5 @@
 
-var news_column1_pdf1 = "Spass am Pferd";
+var news_column1_pdf1 = "";
 
 var news_column1_pdf2 = "";
 
@@ -17,7 +17,7 @@ var news_column3_pdf2 = "";
 
 var news_column3_pdf3 = "";
 
-var news_column4_pdf1 = "Stangen-Trail-Training1";
+var news_column4_pdf1 = "";
 
 var news_column4_pdf2 = "";
 

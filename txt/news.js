b9ada42b1b69745e8baa51730cef817564ed6f2d
pferdherz <58362627+pferdherz@users.noreply.github.com>
2021-04-04 13:42:10 +0200

    Update news.js

--- txt/news.js
+++ txt/news.js
@@ -4,7 +4,7 @@ var news_titel = "Aktuelles";
 var news_text1 = "Hier finden Sie akutelle Informationen zu J&S Kursen, Reittagen, Preislisten und weitere News.";
 var news_column1_titel = "J&S Kurse";
 
-var news_column1_text1 = "Freie Unterrichtstunden:<br> Montag 14.00-15.00 Uhr <br>Mittwoch von 15.30-16.30";
+var news_column1_text1 = "Freie Unterrichtstunden:<br> Montag 14.00-15.00 Uhr <br>Mittwoch von 15.30-16.30<br>Bei Fragen einfach melden.<br><br>Während den Schulferien können wir Ihnen ab 2-3 Kindern ein individuelles Programm zusammenstellen.<br>Nehmen Sie Kontakt mit uns auf!";
 var news_column1_text2 = " ";
 var news_column1_text3 = " ";
 
@@ -21,7 +21,7 @@ var news_column2_text4 = " ";
 
 var news_column3_titel = "Preise & AGBs";
 
-var news_column3_text1 = "Kinderreitstunde Privat <br>45.- (50 Min) <br><br> Erwachsene Privat<br> 50.- (50 Min.) <br><br> SURE FOOT<sup>&reg;</sup><br>60.- (50-60 Min) <br> 3er Abo 160.- <br><br>Bei mobilem Unterricht kommen Fahrkosten dazu. <br><br>Im Verhinderungsfall bitte 24 Stunden im Voraus abmelden, sonst muss die Stunde verechnet werden.<br><br>Das Tragen eines Reithelms für Kinder ist Pflicht.<br><br>Das Aufbauen und Aufräumen von Hindernissen wird grundsätzlich in die Lektionenzeit eingerechnet. Geimeinsam geht es schneller.<br><br> Es wird jegliche Haftung abgelehnt.";
+var news_column3_text1 = "Kinderreitstunde Privat <br>45.- (50 Min) <br><br> Erwachsene Privat<br> 50.- (50 Min.) <br><br> SURE FOOT<sup>&reg;</sup><br>60.- (50-60 Min) <br> 3er Abo 160.- <br><br>Bei mobilem Unterricht kommen Fahrkosten dazu.<br><br>Mobiler Unterricht und Teilnahme an Kursen nur mit gesunden Pferden aus einem seuchenfreien Stall buchen und besuchen. <br><br>Im Verhinderungsfall bitte 24 Stunden im Voraus abmelden, sonst wird die Stunde verechnet.<br><br>Das Tragen eines Reithelms für Kinder ist Pflicht.<br><br>Das Aufbauen und Aufräumen von Hindernissen wird grundsätzlich in die Lektionenzeit eingerechnet. Geimeinsam geht es schneller.<br><br> Es wird jegliche Haftung abgelehnt.";
 
 var news_column3_text2 = " ";
 

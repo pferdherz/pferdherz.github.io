d90b36d7082d4ac8d6abece515b4b9f7ef327b86
pferdherz <58362627+pferdherz@users.noreply.github.com>
2021-04-04 20:12:36 +0200

    Update pics.js

--- imgs/gallery/pics.js
+++ imgs/gallery/pics.js
@@ -3,6 +3,6 @@ var pics1 = ["springen", "kurs", "abend2", "agility", "avenches", "hut"];
 
 var pics2 = ["tofflan", "brücke", "", "jura"];
 
-var pics3 = ["tor", "chiccajura", "chiccaturnier", "zebra", "", "", "schnee1", "kopf"];
+var pics3 = ["tor", "chiccajura", "chiccaturnier", "zebra", "schnee1", "kopf"];
 
-var pics4 = ["yanis2", "wald", "galerie1", "longe", "anjafiona,jpeg"];
+var pics4 = ["yanis2", "wald", "galerie1", "longe", "anjafiona"];

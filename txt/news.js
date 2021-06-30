ff992c01adbaf4811001ba8580d991debc92bd3a
pferdherz <58362627+pferdherz@users.noreply.github.com>
2021-06-30 10:55:54 +0200

    Update news.js

--- txt/news.js
+++ txt/news.js
@@ -4,7 +4,7 @@ var news_titel = "Aktuelles";
 var news_text1 = "Hier finden Sie akutelle Informationen zu J&S Kursen, Reittagen, Preislisten und weitere News.";
 var news_column1_titel = "J&S Kurse";
 
-var news_column1_text1 = "Freie Unterrichtstunden:<br><br> Montag <br>14.00-15.00 Uhr <br><br>Mittwoch 15.30-16.30 Uhr<br>und auf Anfrage<br><br>Bei Fragen einfach melden.<br><br>Während den Schulferien können wir Ihnen ab 2-3 Kindern ein individuelles Programm zusammenstellen.<br>Nehmen Sie Kontakt mit uns auf!";
+var news_column1_text1 = "Freie Unterrichtstunden:<br><br> Montag <br>14.00-15.00 Uhr <br><br>Mittwoch Nachmittag <br>1 Platz in einer 2er-Gruppe Uhr<br>und auf Anfrage<br><br>Bei Fragen einfach melden.<br><br>Während den Schulferien können wir Ihnen ab 2-3 Kindern ein individuelles Programm zusammenstellen.<br>Nehmen Sie Kontakt mit uns auf!";
 var news_column1_text2 = " ";
 var news_column1_text3 = " ";
 
@@ -21,7 +21,7 @@ var news_column2_text4 = " ";
 
 var news_column3_titel = "Preise & AGBs";
 
-var news_column3_text1 = "Kinderreitstunde Privat <br>45.- (50 Min) <br><br> Erwachsene Privat<br> 50.- (50 Min.) <br><br> SURE FOOT<sup>&reg;</sup><br>60.- (50-60 Min) <br> 3er Abo 160.- <br><br>Bei mobilem Unterricht kommen Fahrkosten dazu.<br><br>Mobiler Unterricht und Teilnahme an Kursen nur mit gesunden Pferden aus einem seuchenfreien Stall buchen und besuchen. <br><br>Im Verhinderungsfall bitte 24 Stunden im Voraus abmelden, sonst wird die Stunde verechnet.<br><br>Das Tragen eines Reithelms für Kinder ist Pflicht.<br><br>Das Aufbauen und Aufräumen von Hindernissen wird grundsätzlich in die Lektionenzeit eingerechnet. Geimeinsam geht es schneller.<br><br> Es wird jegliche Haftung abgelehnt.";
+var news_column3_text1 = "Kinderreitstunde Privat <br>45.- (50 Min) <br><br> 2-er Gruppe Kinder<br>30.- (60 Min.)<br><br>Erwachsene Privat<br> 50.- (50 Min.) <br><br> SURE FOOT<sup>&reg;</sup><br>60.- (50-60 Min) <br> 3er Abo 160.- <br><br>Bei mobilem Unterricht kommen Fahrkosten dazu.<br><br>Mobiler Unterricht und Teilnahme an Kursen nur mit gesunden Pferden aus einem seuchenfreien Stall buchen und besuchen. <br><br>Im Verhinderungsfall bitte 24 Stunden im Voraus abmelden, sonst wird die Stunde verechnet.<br><br>Das Tragen eines Reithelms für Kinder ist Pflicht.<br><br>Das Aufbauen und Aufräumen von Hindernissen wird grundsätzlich in die Lektionenzeit eingerechnet. Geimeinsam geht es schneller.<br><br> Es wird jegliche Haftung abgelehnt.";
 
 var news_column3_text2 = " ";
 
@@ -31,8 +31,8 @@ var news_column3_text4 = " ";
 
 var news_column4_titel = "Sonstiges";
 
-var news_column4_text1 = "Aufgrund der Corona und Herpes Situation finden Kurse spontan statt. <br>Kurse bei euch zu Hause z.B. Bodenarbeit, Sitzkurs oder SURE FOOT sind jederzeit möglich! Gerne bringe ich auch Material mit.<br>Ich freue mich auf euer Kontaktaufnahme";
-var news_column4_text2 = "März 2021 <br> Ende März durfte ich in Moosseedorf einen Stall internen Bodenarbeitskurs durchführen.<br>Da das Wetter schön war, konnten wir den Kurs draussen auf einer grossen Weide durchführen und uns jederzeit an die Coronaregeln halten. <br> Wir starteten mit einer 30 minütigen Theoriesequenz. <br>Danach arbeitete die erste Gruppe an der Basis der Bodenschule und wandte die erarbeitete Theorie an. Das Ganze übten wir dann mit tollen Ponys / Pferden und motiverten Führerinnen in einem Parcour. Die zweite Gruppe schaute mit entsprechendem Abstand zu. Nach einer Stunde gab es dann einen Gruppenwechsel. <br>So konnten wir den Kurs mit zufriedener Kursleiterin & Teilnehmer abschliessen.<br><br>Winter 20/21<br>Den ruhigen Winter nutze ich für eine Online-Weiterbildung. Ich habe mein Wissen zum Thema Pferdeanatomie und Biomechanik aufgefrischt und vertieft. Ich freue mich darauf, mehr zum Thema Biomechanik in meinen Unterricht mit euch einzubauen.<br><br> 17.08.20<br>Zwei spannende und lehrreiche Kindersport Weiterbildungstage im NPZ Bern liegen hinter mir. Es wurde viel Wissen um den Aufbau von Kindersportlektionen und Dressurlektionen aufgefrischt, Demo-Lektionen abgehalten und GA03 geritten und gerichtet. Ich freue mich, die neuen Inputs in meinen Reitstunden einzubauen.";
+var news_column4_text1 = "Kurse bei euch zu Hause z.B. Bodenarbeit, Sitzkurs oder SURE FOOT sind jederzeit möglich! Gerne bringe ich auch Material mit.<br>Ich freue mich auf euer Kontaktaufnahme";
+var news_column4_text2 = "Neue Stangen-Gymkhana-Trainings<br>10. Juli & 24. Juli<br>08.00-09.00 Uhr in Schönbühl.<br>Anmeldung via Whatsapp, Threema oder Mail.<br><br>Winter 20/21<br>Den ruhigen Winter nutze ich für eine Online-Weiterbildung. Ich habe mein Wissen zum Thema Pferdeanatomie und Biomechanik aufgefrischt und vertieft. Ich freue mich darauf, mehr zum Thema Biomechanik in meinen Unterricht mit euch einzubauen.<br><br> 17.08.20<br>Zwei spannende und lehrreiche Kindersport Weiterbildungstage im NPZ Bern liegen hinter mir. Es wurde viel Wissen um den Aufbau von Kindersportlektionen und Dressurlektionen aufgefrischt, Demo-Lektionen abgehalten und GA03 geritten und gerichtet. Ich freue mich, die neuen Inputs in meinen Reitstunden einzubauen.";
 
 var news_column4_text3 = " ";
 

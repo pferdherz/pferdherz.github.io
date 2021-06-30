47ecbb5ceaafd8edecef768d5514fdf41aa6e146
pferdherz <58362627+pferdherz@users.noreply.github.com>
2021-06-30 10:59:07 +0200

    Update news.js

--- txt/news.js
+++ txt/news.js
@@ -4,7 +4,7 @@ var news_titel = "Aktuelles";
 var news_text1 = "Hier finden Sie akutelle Informationen zu J&S Kursen, Reittagen, Preislisten und weitere News.";
 var news_column1_titel = "J&S Kurse";
 
-var news_column1_text1 = "Freie Unterrichtstunden:<br><br> Montag <br>14.00-15.00 Uhr <br><br>Mittwoch Nachmittag <br>1 Platz in einer 2er-Gruppe Uhr<br>und auf Anfrage<br><br>Bei Fragen einfach melden.<br><br>Während den Schulferien können wir Ihnen ab 2-3 Kindern ein individuelles Programm zusammenstellen.<br>Nehmen Sie Kontakt mit uns auf!";
+var news_column1_text1 = "Freie Unterrichtstunden:<br><br> Montagnachmittag <br><br>Mittwoch Nachmittag <br>1 Platz in einer 2er-Gruppe Uhr<br>oder auf Anfrage<br><br>Bei Fragen einfach melden.<br><br>Während den Schulferien können wir Ihnen ein individuelles Programm zusammenstellen.<br>Nehmen Sie Kontakt mit uns auf!";
 var news_column1_text2 = " ";
 var news_column1_text3 = " ";
 
@@ -31,8 +31,8 @@ var news_column3_text4 = " ";
 
 var news_column4_titel = "Sonstiges";
 
-var news_column4_text1 = "Kurse bei euch zu Hause z.B. Bodenarbeit, Sitzkurs oder SURE FOOT sind jederzeit möglich! Gerne bringe ich auch Material mit.<br>Ich freue mich auf euer Kontaktaufnahme";
-var news_column4_text2 = "Neue Stangen-Gymkhana-Trainings<br>10. Juli & 24. Juli<br>08.00-09.00 Uhr in Schönbühl.<br>Anmeldung via Whatsapp, Threema oder Mail.<br><br>Winter 20/21<br>Den ruhigen Winter nutze ich für eine Online-Weiterbildung. Ich habe mein Wissen zum Thema Pferdeanatomie und Biomechanik aufgefrischt und vertieft. Ich freue mich darauf, mehr zum Thema Biomechanik in meinen Unterricht mit euch einzubauen.<br><br> 17.08.20<br>Zwei spannende und lehrreiche Kindersport Weiterbildungstage im NPZ Bern liegen hinter mir. Es wurde viel Wissen um den Aufbau von Kindersportlektionen und Dressurlektionen aufgefrischt, Demo-Lektionen abgehalten und GA03 geritten und gerichtet. Ich freue mich, die neuen Inputs in meinen Reitstunden einzubauen.";
+var news_column4_text1 = "Neue Stangen-Gymkhana-Trainings<br>10. Juli & 24. Juli 2021<br>08.00-09.00 Uhr in Schönbühl.<br>Anmeldung via Whatsapp, Threema oder Mail.<br><br>Kurse bei euch zu Hause z.B. Bodenarbeit, Sitzkurs oder SURE FOOT sind jederzeit möglich! Gerne bringe ich auch Material mit.<br>Ich freue mich auf euer Kontaktaufnahme";
+var news_column4_text2 = "Winter 20/21<br>Den ruhigen Winter nutze ich für eine Online-Weiterbildung. Ich habe mein Wissen zum Thema Pferdeanatomie und Biomechanik aufgefrischt und vertieft. Ich freue mich darauf, mehr zum Thema Biomechanik in meinen Unterricht mit euch einzubauen.<br><br> 17.08.20<br>Zwei spannende und lehrreiche Kindersport Weiterbildungstage im NPZ Bern liegen hinter mir. Es wurde viel Wissen um den Aufbau von Kindersportlektionen und Dressurlektionen aufgefrischt, Demo-Lektionen abgehalten und GA03 geritten und gerichtet. Ich freue mich, die neuen Inputs in meinen Reitstunden einzubauen.";
 
 var news_column4_text3 = " ";
 
